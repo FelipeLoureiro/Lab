0e391e04202db198fdcc298d680e8d953018beec
FelipeLoureiro <felipe.loureiro@before.com.br>
2019-04-29 11:22:16 -0400

    Projeto

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class LoginTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('https://syscor-homologacao.before.com.br/_sys/?logUfIdSession=12')
+                    ->assertSee('Bem-vindo ao software de gestão de revendas SysCor Vivo GO.');
+        });
+    }
+}
